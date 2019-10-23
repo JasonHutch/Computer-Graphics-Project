41b86475792441a208946a4aece4fb467c1cba92
JasonHutch <jrh150730@utdallas.edu>
2019-10-23 15:18:20 -0500

    Project Refacor / Added more platforms
    I accidentally made changes in a version of the project without the .git file so I had to migrate all the files to the correct dirrectory

---
+++ Computer-Graphics-Project/Assets/Scripts/Movement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float jumpForce = 3.0f;
+    public float speed;
+    private float moveInputX;
+    private float moveInputY;
+    private Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        moveInputX = Input.GetAxisRaw("Mouse X");
+        moveInputY = Input.GetAxisRaw("Mouse Y");
+
+        rb.velocity = new Vector2(moveInputX * speed, moveInputY * speed);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.velocity = Vector2.up * jumpForce;
+        }
+    }
+}
