f773649611980932331fc4bfc81ad775b68a1e25
JasonHutch <jrh150730@utdallas.edu>
2019-10-23 08:22:14 -0500

    Added mock character and test platforms

---
+++ Computer Graphics Project/Assets/CameraFollow2.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow2 : MonoBehaviour
+{
+    private Transform playerTransform;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+        
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        //current camera position
+        Vector3 temp = transform.position;
+        //set camera x to equal player x
+        temp.x = playerTransform.position.x;
+        temp.y = playerTransform.position.y;
+
+        transform.position = temp;
+    }
+}
