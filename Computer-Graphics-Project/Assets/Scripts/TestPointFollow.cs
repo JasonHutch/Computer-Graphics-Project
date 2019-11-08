91e5d6c5147b230645caf7ba2cb13656b0a50b35
JasonHutch <jrh150730@utdallas.edu>
2019-11-08 16:55:57 -0600

    treid to fix player movement

---
+++ Computer-Graphics-Project/Assets/Scripts/TestPointFollow.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+public class TestPointFollow : MonoBehaviour
+{
+    public float speed = 10f;
+    private Vector3 targetPosition;
+    private bool isMoving;
+    public GameObject clickAnimation;
+    private Rigidbody2D rb;
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 temp = Input.mousePosition;
+        temp.z = 0.0f; // Set this to be the distance you want the object to be placed in front of the camera.
+        this.transform.position = Camera.main.ScreenToWorldPoint(temp);
+    }
+}
